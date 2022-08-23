a9e2d01dba6bb576ffdcd0a2befa3e726e5bc5c6
ronald chiagozie <gronaldchia@gmail.com>
2022-08-23 23:15:45 +0100

    signature

---
+++ crowdfunding-product-page-main/app.js
@@ -0,0 +1,10 @@
+const back = document.querySelector("#back")
+const modals = document.querySelector("#modals")
+const closemodal = document.querySelector("#closemodal")
+const bookmark = document.querySelector("bookmark")
+function OpenModal() {
+    console.log("fdgdgh")
+    modals.classList.add("show")
+}
+
+
